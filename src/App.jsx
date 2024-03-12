6e8aa590d040a0eeace2c359dfba7d46b59e2973
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-03-12 21:39:31 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div className="main-header">
+      <img className="img" src={logo} alt="logo"/>
+      <div className="header-text">
+        <h2>Modestas Juska</h2>
+        <h5>Product-focused Fullstack Developer</h5>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App;
